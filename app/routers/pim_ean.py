6ac855f1988c0786cd3fa7f023c6343e567bfe01
Mike Gugel <mikhail.gugel@bahag.com>
2022-04-19 11:44:33 +0200

    Reference updated, pim2ean conversion using the single bdx-data

--- app/routers/pim_ean.py
+++ app/routers/pim_ean.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 
-from app.models.reference import PimEan
+from app.models.reference import PimQuery20_5
 
 from app.utils.dependencies import verify_token
 
@@ -11,5 +11,13 @@ router = APIRouter()
 @router.get('/pim_ean/{query}')
 async def lookup(query: str, dependencies=Depends(verify_token)):
     if len(query) == 13:
-        return await PimEan.objects.get_or_none(ean=query)
-    return await PimEan.objects.get_or_none(variant_product=query)
+        resp =  await PimQuery20_5.objects.get_or_none(EAN=query)
+    else:
+        resp = await PimQuery20_5.objects.get_or_none(Variant_product=query)
+
+    return {
+        'variant_product': resp.Variant_product,
+        'base_product': resp.Base_product,
+        'material_group': resp.Material_group.split(' - ')[0],
+        'ean': resp.EAN,
+    }
