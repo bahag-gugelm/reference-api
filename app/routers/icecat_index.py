f7074120844e75f72164937055fdd0157d27db13
bahag-fadinaa <adeolu-samuel.fadina@bahag.com>
2022-06-09 16:17:54 +0200

    update icecat_index router

--- app/routers/icecat_index.py
+++ app/routers/icecat_index.py
@@ -1,14 +1,17 @@
 from fastapi import APIRouter, Depends
-from app.models.reference import IcecatIndex
+from app.models.reference import IcecatSKU, IcecatEAN
 from app.utils.dependencies import verify_token
+import json
 
 router = APIRouter()
 
 
 @router.get('/sku_ean')
 async def lookup(sku: str, brand: str, dependencies=Depends(verify_token)):
-    resp = await IcecatIndex.objects.get_or_none(sku=sku, brand=brand)
-    if resp:
-        return resp
+    sku_resp = await IcecatSKU.objects.get_or_none(sku=sku, brand=brand)
+    if sku_resp:
+        icecat_id = json.loads(sku_resp.json()).get("id")
+        ean_resp = await IcecatEAN.objects.all(id=icecat_id)
+        return ean_resp
     else:
         return None
