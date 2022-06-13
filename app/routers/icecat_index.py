9bd24403b347847e2b7bdfd5e1c4580943cc15b8
bahag-fadinaa <adeolu-samuel.fadina@bahag.com>
2022-06-13 11:51:15 +0200

    couple manufacturer SKU-EAN with internal IDs

--- app/routers/icecat_index.py
+++ app/routers/icecat_index.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
-from app.models.reference import IcecatIndex
+from app.models.reference import IcecatIndex, PimQuery20_5
 from app.utils.dependencies import verify_token
-
+import json
 
 router = APIRouter()
 
@@ -9,4 +9,26 @@ router = APIRouter()
 @router.get('/sku_ean')
 async def lookup(sku: str, brand: str, dependencies=Depends(verify_token)):
     sku_resp = await IcecatIndex.objects.all(sku=sku, brand=brand)
-    return sku_resp
+    response=[]
+    if sku_resp:
+        for item in sku_resp:
+            item = json.loads(item.json())
+            ean = item.get("ean")
+            if len(ean)==13:
+                resp =  await PimQuery20_5.objects.get_or_none(EAN=ean)
+                if resp:
+                    resp.update({
+                        "sku": sku,
+                        "brand": brand
+                    })
+                    response.append(resp)
+                else:
+                    response.append({
+                        "ean": ean,
+                        "variant_product": None,
+                        "base_product": None,
+                        "material_group": None,
+                        "sku": sku,
+                        "brand": brand
+                    })
+    return response
