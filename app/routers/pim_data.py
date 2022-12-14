63135dc279b6d2a348a2ca11126fe0c1edf6e578
Mike Gugel <mikhail.gugel@bahag.com>
2022-12-14 13:05:05 +0100

    get_product_info is now omnivorous

--- app/routers/pim_data.py
+++ app/routers/pim_data.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, Depends
 from app.models.reference import PimQuery20_5, PimQuery29
 
 from app.utils.dependencies import verify_token
+from app.utils.misc import get_q205
 
 
 router = APIRouter()
@@ -10,29 +11,25 @@ router = APIRouter()
 
 @router.get('/pim_ean/{query}')
 async def lookup(query: str, dependencies=Depends(verify_token)):
-    if len(query) == 13:
-        resp =  await PimQuery20_5.objects.get_or_none(EAN=query)
-    else:
-        resp = await PimQuery20_5.objects.get_or_none(Variant_product=query)
-
-    if resp:
+    resp205 = await get_q205(query=query)
+    if resp205:
         return {
-            'variant_product': resp.Variant_product,
-            'base_product': resp.Base_product,
-            'material_group': resp.Material_group.split(' - ')[0],
-            'ean': resp.EAN,
+            'variant_product': resp205.Variant_product,
+            'base_product': resp205.Base_product,
+            'material_group': resp205.Material_group.split(' - ')[0],
+            'ean': resp205.EAN,
         }
 
 
-@router.get('/product/{ean}')
-async def get_product_info(ean: str, dependencies=Depends(verify_token)):
+@router.get('/product/{query}')
+async def get_product_info(query: str, dependencies=Depends(verify_token)):
     attribs_exclude_fields = {
             'id', 'Material_group', 'Variant_product',
             'Mandators', 'SAP_name',
             }
-    res205 = ean and await PimQuery20_5.objects.get_or_none(EAN=ean)
-    res = res205 and res205.dict()
-    if res:
-        res29 = await PimQuery29.objects.all(Variant_product=res['Variant_product'])
-        res['Attributes'] = [item.dict(exclude=attribs_exclude_fields) for item in res29]
-        return res
+    resp205 = await get_q205(query=query)
+    resp205 = resp205 and resp205.dict()
+    if resp205:
+        res29 = await PimQuery29.objects.all(Variant_product=resp205['Variant_product'])
+        resp205['Attributes'] = [item.dict(exclude=attribs_exclude_fields) for item in res29]
+        return resp205
---
+++ app/utils/misc.py
@@ -0,0 +1,7 @@
+from app.models.reference import PimQuery20_5
+
+async def get_q205(query: str):
+    if len(query) == 13:
+        return  await PimQuery20_5.objects.get_or_none(EAN=query)
+    else:
+        return await PimQuery20_5.objects.get_or_none(Variant_product=query)
