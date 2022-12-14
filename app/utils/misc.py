a4429cbd37a4f772fcbdab73fc79c21051438d36
Mike Gugel <mikhail.gugel@bahag.com>
2022-12-14 13:21:26 +0100

    padded eans search fix

--- app/utils/misc.py
+++ app/utils/misc.py
@@ -2,6 +2,6 @@ from app.models.reference import PimQuery20_5
 
 async def get_q205(query: str):
     if len(query) == 13:
-        return  await PimQuery20_5.objects.get_or_none(EAN=query)
+        return  await PimQuery20_5.objects.get_or_none(EAN=query.lstrip('0'))
     else:
         return await PimQuery20_5.objects.get_or_none(Variant_product=query)
